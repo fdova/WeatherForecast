c949ba376e26ca4c5665a5c5104fd176d4ffaa3d
Filia Dova <filia.dova@gmail.com>
2020-03-08 23:44:03 +0200

    fixed test

--- RestApi.Tests/WeatherForecast_Test1.cs
+++ RestApi.Tests/WeatherForecast_Test1.cs
@@ -1,18 +1,34 @@
 using Xunit;
+using RestApi.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace RestApi.UnitTests
 {
+
     public class WeatherForecast_Test1
     {
+        private readonly WeatherForecastController _controller;
+
+        public WeatherForecast_Test1()
+        {
+            _controller = new WeatherForecastController(null);
+        }
+
         [Fact]
         public void TestName()
         {
         //Given
-                
+            List<WeatherForecast> forecast = _controller.Get().ToList();
+            DateTime tomorrow = DateTime.Now.AddDays(1);      
         //When
-        
+            DateTime dt = forecast.FirstOrDefault().Date;
         //Then
-        Assert.True(false);
+        Assert.True(dt.Day == tomorrow.Day);
+        Assert.True(dt.Month == tomorrow.Month);
+        Assert.True(dt.Year == tomorrow.Year);
+
         }
     }
 }
